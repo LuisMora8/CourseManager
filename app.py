a84c4e92e9caad8d0a7237a0642a8089c3fe8a68
Luis Mora <moraluis1325@gmail.com>
2023-04-03 17:38:45 -0700

    Classes and Professor Models

--- app.py
+++ app.py
@@ -3,17 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Flask and SQLDatabase configuaration
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://grades.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///students.sqlite"
 db = SQLAlchemy(app)
 
 
 # Database Models
-
 class Students(db.Model):
     # Static variable to create unique key
     numkey = 0
 
-    student_numkey = db.Column(db.Integer)
+    student_numkey = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
 
@@ -28,10 +27,10 @@ class Grades(db.Model):
     # Static variable to create unique key
     numkey = 0
 
-    grade_numkey = db.Column(db.Integer)
+    grade_numkey = db.Column(db.Integer, primary_key=True)
     student_numkey = db.Column(db.Integer)
     class_numkey = db.Column(db.Integer)
-    grade = db.Column(db.Double)
+    grade = db.Column(db.Float)
 
     def __init__(self, student_key, class_key, grade) -> None:
         super().__init__()
@@ -41,10 +40,50 @@ class Grades(db.Model):
         self.class_numkey = class_key
         self.grade = grade
 
+class Classes(db.Model):
+    # Static variable to create unique key
+    numkey = 0
+
+    class_numkey = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String)
+    prof_numkey = db.Column(db.Integer)
+    start_time = db.Column(db.String)
+    end_time = db.Column(db.String)
+    days = db.Column(db.String)
+    enrolled = db.Column(db.Integer)
+
+    def __init__(self, name, prof_key, start, end, days, enrolled) -> None:
+        super().__init__()
+        Classes.numkey += 1
+        self.class_numkey = Classes.numkey
+        self.class_name = name
+        self.prof_numkey = prof_key
+        self.start_time = start
+        self.end_time = end
+        self.days = days
+        self.enrolled = enrolled
+
+class Professor(db.Model):
+    # Static variable to create unique key
+    numkey = 0
+
+    prof_numkey = db.Column(db.Integer, primary_key=True)
+    prof_name = db.Column(db.Integer)
+    class_numkey = db.Column(db.Integer)
+
+    def __init__(self, name, class_key) -> None:
+        super().__init__()
+        Professor.numkey += 1
+        self.prof_numkey = Professor.numkey
+        self.prof_name = name
+        self.class_numkey = class_key
+
+
 # Home Page
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 # Driver Code
 if __name__ == '__main__':
