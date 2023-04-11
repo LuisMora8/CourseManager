12c4f4598f004c8103ba2e4dd3d655335312fefe
erick <evargas44@ucmerced.edu>
2023-04-11 13:06:11 -0700

    added text input to table

--- static/showClassesGrades.js
+++ static/showClassesGrades.js
@@ -1,13 +1,16 @@
 function displayClassGrades(data) {
-    
+    let x = 1;
     console.log(data);
     let table = '<table border="1">';
     table += `<tr><th>Student Name</th><th>Grades</th>`;
     data.forEach((data, index) => {
         table = table + `<tr>`;
         table = table + `<td>${data.studentName}</td>`;
-        table = table + `<td>${data.grades}</td>`;
+        table = table + `<td>
+        <input type = 'text'  id = 'textEdit${{x}}' value = '${data.grades}'>
+        </td>`;
         table += `</tr>`;
+        x=x+1;
       });
       table += "</table>";
       document.getElementById("studentGradeslist").innerHTML = table;
@@ -17,3 +20,15 @@ function displayClassGrades(data) {
     alert(id)
     console.log(id)
   }
+
+function submitFunction(data){
+    // var xhttp = new XMLHttpRequest();
+    // data.forEach((data,index)=>{
+    //     var test = document.getElementById('textEdit').value;
+    //     console.log(test)
+    // })
+    var test = document.getElementById('textEdit1').value;
+
+    console.log(test);
+
+}
