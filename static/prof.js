f0696f34880879dc4cc5ffd388325da9488010f7
erick <evargas44@ucmerced.edu>
2023-04-11 16:53:50 -0700

    fixed merge ploblems

--- static/prof.js
+++ static/prof.js
@@ -1,11 +1,17 @@
 // Display the courses as a table
+let x =1
+// table = table + `<td>
+        // <a href={{ url_for('/classGrade', name=${data.id}) }}">${data.id}</a>
+        // </td>`;
 function displayProfessors(data) {
+    console.log(x)
+    x= x+1
     console.log(data);
     let table = '<table border="1">';
     table += `<tr><th>Course Name</th><th>Professor</th><th>Time</th><th>Enrollment</th></tr>`;
     data.forEach((data, index) => {
         table = table + `<tr>`;
-        table = table + `<td>${data.id}</td>`;
+        table = table + `<td><a href="/classGrades/${data.id}">${data.className}</a></td>`;
         table = table + `<td>${data.profName}</td>`;
         table = table + `<td>${data.time}</td>`;
         table = table + `<td>${data.enrolled}</td>`;
@@ -15,3 +21,37 @@ function displayProfessors(data) {
       document.getElementById("Proflist").innerHTML = table;
       console.log("displayProfessors");
   }
+
+
+  // function showClass(id){
+  //   var xhttp = new XMLHttpRequest();
+  //   xhttp.open("GET","http://127.0.0.1:5000/classGrades")
+  //   xhttp.onload = function(){
+  //       console.log("testing show classes")
+  //       window.alert("hello")
+  //   };
+  //   xhttp.send();
+  // }
+
+
+  // function displayClassGrades(data) {
+  //   console.log(x)
+  //   x= x+1
+  //   console.log(data);
+  //   let table = '<table border="1">';
+  //   table += `<tr><th>Course Name</th><th>Professor</th><th>Time</th><th>Enrollment</th></tr>`;
+  //   data.forEach((data, index) => {
+  //       table = table + `<tr>`;
+  //       table = table + `<td>${data.profName}</td>`;
+  //       table = table + `<td>${data.time}</td>`;
+  //       table = table + `<td>${data.enrolled}</td>`;
+  //       table += `</tr>`;
+  //     });
+  //     table += "</table>";
+  //     document.getElementById("Proflist").innerHTML = table;
+  //     console.log("displayProfessors");
+  // }
+  // function openClass(id){
+  //   alert(id)
+  //   console.log(id)
+  // }
---
+++ static/showClassesGrades.js
@@ -0,0 +1,46 @@
+function displayClassGrades(data) {
+    let x = 1;
+    console.log(data);
+    let table = '<table border="1">';
+    table += `<tr><th>Student Name</th><th>Grades</th>`;
+    data.forEach((data, index) => {
+        table = table + `<tr>`;
+        table = table + `<td>${data.studentName}</td>`;
+        table = table + `<td>
+        <input type = 'text'  id = 'textEdit${x}' value = '${data.grades}'>
+        </td>`;
+        table = table + `<td><button onclick="edit('${data.studentName}','${x}')">edit</button></td>`;
+        table += `</tr>`;
+        x=x+1;
+      });
+      table += "</table>";
+      document.getElementById("studentGradeslist").innerHTML = table;
+      console.log("displayStudentGrades");
+  }
+//   function openClass(id){
+//     alert(id)
+//     console.log(id)
+//   }
+function edit(name,id){
+    console.log(name);
+    console.log(id)
+    console.log(document.getElementById("textEdit"+id).value)
+    let grade = document.getElementById("textEdit"+id).value
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET","http://127.0.0.1:5000/"+name+"/"+grade);
+    xhttp.send();
+    // console.log(id)
+}
+function submitFunction(data){
+    // var xhttp = new XMLHttpRequest();
+    // data.forEach((data,index)=>{
+    //     var test = document.getElementById('textEdit').value;
+    //     console.log(test)
+    // })
+    var test = document.getElementById('textEdit1').value;
+
+
+    console.log(test);
+
+
+}
